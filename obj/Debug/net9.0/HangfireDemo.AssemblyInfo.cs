bb3c0e17e9dc95765e1bfd0aaa72ff02eec2d636
Aryan Prajapati <aryanprajapati2207@gmail.com>
2025-12-15 15:51:10 +0530

    hide credentials

--- obj/Debug/net9.0/HangfireDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/HangfireDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HangfireDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c1dd96fe75effd028d7c85f02442a1d6fa5c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("HangfireDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HangfireDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
